d50a49f82555f009f92a405b49bca37c05635659
Adam Kurbiel <ad.kurbiel@gmail.com>
2026-06-07 13:15:48 +0200

    player doesn't fall until he clicks

--- src/entities/bird.js
+++ src/entities/bird.js
@@ -1,7 +1,7 @@
 export class Bird {
     constructor() {
-        this.x = 200;
-        this.y = 200;
+        this.x = 225;
+        this.y = 225;
         this.width = 32;
         this.height = 32;
         this.color = "green";
@@ -10,9 +10,13 @@ export class Bird {
         this.gravity = 0.6;
         this.jumpStrength = -10;
         this.terminalVelocity = 12;
+
+        this.started = false
     }
 
     update() {
+        if (this.y > 500 || this.started == false)  return;
+
         this.velocity += this.gravity;
         if (this.velocity > this.terminalVelocity) {
             this.velocity = this.terminalVelocity;
@@ -27,6 +31,12 @@ export class Bird {
     }
 
     jump() {
+        this.started = true
+        if (this.y > 500){
+            this.y = 500;
+            this.velocity = 0;
+            return;
+        }
         this.velocity = this.jumpStrength;
     }
 }
